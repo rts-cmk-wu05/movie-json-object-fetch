dd8653a7f16ee2294b17d5d1d91f3402011a3512
Robert <robertwattboolsen@gmail.com>
2021-03-02 15:22:10 +0100

    Øvelse 4

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const movieList = [
         "Genre": ["Drama", "Western"],
         "Director": "Quentin Tarantino",
         "Writer": "Quentin Tarantino",
-        "Actors": ["Jamie Foxx", "Christoph Waltz", "Leonardo DiCaprio", "Kerry Washington"]
+        "Actors": ["Jamie Foxx", " Christoph Waltz", " Leonardo DiCaprio", " Kerry Washington"]
     },
     {
         "Title": "Brazil",
@@ -16,7 +16,7 @@ const movieList = [
         "Genre": ["Drama", "Sci-Fi"],
         "Director": "Terry Gilliam",
         "Writer": "Terry Gilliam",
-        "Actors": ["Jonathan Pryce", "Robert De Niro", "Katherine Helmond", "Ian Holm"]
+        "Actors": ["Jonathan Pryce", " Robert De Niro", " Katherine Helmond", " Ian Holm"]
     },
     {
         "Title": "American History X",
@@ -25,7 +25,7 @@ const movieList = [
         "Genre": ["Drama"],
         "Director": "Tony Kaye",
         "Writer": "David McKenna",
-        "Actors": ["Edward Norton", "Edward Furlong", "Beverly D'Angelo", "Jennifer Lien"]
+        "Actors": ["Edward Norton", " Edward Furlong", " Beverly D'Angelo", " Jennifer Lien"]
     },
     {
         "Title": "Fight Club",
@@ -34,7 +34,7 @@ const movieList = [
         "Genre": ["Drama"],
         "Director": "David Fincher",
         "Writer": "Chuck Palahniuk",
-        "Actors": ["Edward Norton", "Brad Pitt", "Meat Loaf", "Zach Grenier"]
+        "Actors": ["Edward Norton", " Brad Pitt", " Meat Loaf", " Zach Grenier"]
     },
     {
         "Title": "Inglourious Basterds",
@@ -43,14 +43,32 @@ const movieList = [
         "Genre": ["Adventure", "Drama", "War"],
         "Director": "Quentin Tarantino",
         "Writer": "Quentin Tarantino",
-        "Actors": ["Brad Pitt", "Mélanie Laurent", "Christoph Waltz", "Eli Roth"]
+        "Actors": ["Brad Pitt", " Mélanie Laurent", " Christoph Waltz", " Eli Roth"]
     }
 ]
 
-console.log(movieList);
+const wrapper = document.querySelector(".wrapper");
 
-for (let i= 0 ; i < movieList.length ; i++){
-    console.log(movieList[i].Title);
-    console.log(movieList[i].Genre[0]+" "+movieList[i].Genre[1]);
-}
+addDataToHTML();
+function addDataToHTML(){
+
+    for (let i = 0; i < movieList.length ; i++){
+
+        const movie = movieList[i];
+        const movieKey = Object.keys(movie); 
+
+        const section = document.createElement('section');
+        section.classList.add('movie-info-box');
+        section.innerHTML =
+            `<h1 class="movie-info-box__header">${movie.Title}</h1>
+            <p class="movie-info-box__info"><span>${movieKey[1]}</span>: ${movie.Year}</p>
+            <p class="movie-info-box__info"><span>${movieKey[2]}</span>: ${movie.Rated}</p>
+            <p class="movie-info-box__info"><span>${movieKey[3]}</span>: ${movie.Genre}</p>
+            <p class="movie-info-box__info"><span>${movieKey[4]}</span>: ${movie.Director}</p>
+            <p class="movie-info-box__info"><span>${movieKey[5]}</span>: ${movie.Writer}</p>
+            <p class="movie-info-box__info"><span>${movieKey[6]}</span>: ${movie.Actors}</p>`
+        wrapper.appendChild(section); 
+    }
+
+};
 
