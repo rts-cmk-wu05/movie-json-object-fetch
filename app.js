68fdf816874163dcc9b86cddebbd248c69b815b5
Robert <robertwattboolsen@gmail.com>
2021-03-03 09:28:30 +0100

    Øvelse 5 og Øvelse 6

--- app.js
+++ app.js
@@ -1,56 +1,7 @@
 
-const movieList = [
-    {
-        "Title": "Django Unchained",
-        "Year": "2012",
-        "Rated": "R",
-        "Genre": ["Drama", "Western"],
-        "Director": "Quentin Tarantino",
-        "Writer": "Quentin Tarantino",
-        "Actors": ["Jamie Foxx", "Christoph Waltz", "Leonardo DiCaprio", "Kerry Washington"]
-    },
-    {
-        "Title": "Brazil",
-        "Year": "1985",
-        "Rated": "R",
-        "Genre": ["Drama", "Sci-Fi"],
-        "Director": "Terry Gilliam",
-        "Writer": "Terry Gilliam",
-        "Actors": ["Jonathan Pryce", "Robert De Niro", "Katherine Helmond", "Ian Holm"]
-    },
-    {
-        "Title": "American History X",
-        "Year": "1998",
-        "Rated": "R",
-        "Genre": ["Drama"],
-        "Director": "Tony Kaye",
-        "Writer": "David McKenna",
-        "Actors": ["Edward Norton", "Edward Furlong", "Beverly D'Angelo", "Jennifer Lien"]
-    },
-    {
-        "Title": "Fight Club",
-        "Year": "1999",
-        "Rated": "R",
-        "Genre": ["Drama"],
-        "Director": "David Fincher",
-        "Writer": "Chuck Palahniuk",
-        "Actors": ["Edward Norton", "Brad Pitt", "Meat Loaf", "Zach Grenier"]
-    },
-    {
-        "Title": "Inglourious Basterds",
-        "Year": "2009",
-        "Rated": "R",
-        "Genre": ["Adventure", "Drama", "War"],
-        "Director": "Quentin Tarantino",
-        "Writer": "Quentin Tarantino",
-        "Actors": ["Brad Pitt", "Mélanie Laurent", "Christoph Waltz", "Eli Roth"]
-    }
-]
-
 const wrapper = document.querySelector(".wrapper");
 
-addDataToHTML();
-function addDataToHTML() {
+function addDataToHTML(movieList) {
 
     for (let i = 0; i < movieList.length; i++) {
 
@@ -77,3 +28,11 @@ function addDataToHTML() {
         wrapper.appendChild(section);
     }
 };
+
+fetchData();
+async function fetchData() {
+    const response = await fetch('movie.json')
+    const result = await response.json()
+    
+    addDataToHTML(result)
+}
